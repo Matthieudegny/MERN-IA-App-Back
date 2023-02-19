6f3081ab186ef4b02d1c96746f26d824e449db12
Matthieudegny <matthieu.dev.degny@gmail.com>
2023-02-19 17:13:08 +0100

    vercel release V2

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
-import connectDB from "./mongodb/connect.js";
-import postRoutes from "./routes/postRoutes.js";
-import dalleRoutes from "./routes/dalleRoutes.js";
+import connectDB from "./api/mongodb/connect.js";
+import postRoutes from "./api/routes/postRoutes.js";
+import dalleRoutes from "./api/routes/dalleRoutes.js";
 
 dotenv.config();
 
